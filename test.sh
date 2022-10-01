db4e152a741279a73e02f615b790db5ddf66a33b
wzwsaint <113655338+wzwsaint@users.noreply.github.com>
2022-10-01 11:41:59 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
-count=0;
-until [[ "$?" -ne 0 ]];
-do
-	count=$((count+1));
-	./detect.sh &>error.txt
-done
-echo "found error after $count runs"
-cat error.txt
+export MARCO=$(pwd)
+echo $(pwd)
+
+
